a001c93051f8788157954f09d2bcaf97d7930d19
nguyenhoanganh26 <n24dtcn002@student.ptithcm.edu.vn>
2026-07-13 09:06:34 +0700

    second

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+pymysql//:root"
+DATABASE_URL = "mysql+pymysql//:root@123456:task_db"
 
 engine = create_engine(DATABASE_URL)
 
